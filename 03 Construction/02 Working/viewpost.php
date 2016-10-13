70d955daec2a0debd5d7bf9759a901b6ab59f645
Edi Prayitno <its.edi@gmail.com>
2016-10-13 17:51:04 +0800

    Develop Comment Feature with Disqus

--- 03 Construction/02 Working/viewpost.php
+++ 03 Construction/02 Working/viewpost.php
@@ -35,6 +35,16 @@ if($row['postID'] == ''){
 				echo '<p>'.$row['postCont'].'</p>';				
 			echo '</div>';
 		?>
+		<div id='disqus_thread'></div>
+		<script type='text/javascript'>
+		  /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+		  var disqus_shortname = 'scmblog2016'; // required: replace example with your forum shortname
+			  (function() {
+			  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		  })();
+		</script>
 
 	</div>
 
