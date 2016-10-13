7ee7c7eba5f948eb5718f5c51c6c15f0304f5b11
Edi Prayitno <its.edi@gmail.com>
2016-10-13 18:00:51 +0800

    Develop Pagination Feature

--- 03 Construction/02 Working/viewpost.php
+++ 03 Construction/02 Working/viewpost.php
@@ -1,7 +1,9 @@
 <?php require('includes/config.php'); 
 
-$stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
-$stmt->execute(array(':postID' => $_GET['id']));
+
+
+$stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts_seo WHERE postSlug = :postSlug');
+$stmt->execute(array(':postSlug' => $_GET['id']));
 $row = $stmt->fetch();
 
 //if post does not exists redirect user.
@@ -27,16 +29,40 @@ if($row['postID'] == ''){
 		<hr />
 		<p><a href="./">Blog Index</a></p>
 
+		<div id='main'>
 
-		<?php	
-			echo '<div>';
-				echo '<h1>'.$row['postTitle'].'</h1>';
-				echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
-				echo '<p>'.$row['postCont'].'</p>';				
-			echo '</div>';
-		?>
+			<?php	
+				echo '<div>';
+					echo '<h1>'.$row['postTitle'].'</h1>';
+					echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).' in ';
+
+						$stmt2 = $db->prepare('SELECT catTitle, catSlug	FROM blog_cats, blog_post_cats WHERE blog_cats.catID = blog_post_cats.catID AND blog_post_cats.postID = :postID');
+						$stmt2->execute(array(':postID' => $row['postID']));
+
+						$catRow = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+
+						$links = array();
+						foreach ($catRow as $cat)
+						{
+						    $links[] = "<a href='c-".$cat['catSlug']."'>".$cat['catTitle']."</a>";
+						}
+						echo implode(", ", $links);
+
+					echo '</p>';
+					echo '<p>'.$row['postCont'].'</p>';				
+				echo '</div>';
+			?>
+
+		</div>
+
+		<div id='sidebar'>
+			<?php require('sidebar.php'); ?>
+		</div>
+
+		<div id='clear'></div>
 
 	</div>
+
 
 </body>
 </html>
